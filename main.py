b161bcb09592093d48e9db73119769d9272013d7
Joana-Esgalhado <joanaesgalhado@gmail.com>
2017-03-12 17:44:59 +0000

    favicon

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ import csv
 borough_data = {}
 
 class BoroughData:
-    def __init__(self, name, recycling_day, frequency, general_waste_days, general_waste_frequency, food_waste_days, food_waste_frequency, materials_provided, contact):
+    def __init__(self, name, recycling_day, recycling_frequency, general_waste_days, general_waste_frequency, food_waste_days, food_waste_frequency, materials_provided, contact):
         self.name = name
         self.recycling_day = recycling_day
-        self.frequency = frequency
+        self.recycling_frequency = recycling_frequency
         self.general_waste_days = general_waste_days
         self.general_waste_frequency = general_waste_frequency
         self.food_waste_days = food_waste_days
@@ -51,6 +51,6 @@ def borough_name(boroughName):
     load_data()
     bdata = find_BoroughData(boroughName)
     print bdata
-    return render_template("recycle.html", recycleday=bdata.recycling_day, frequency=bdata.frequency, generalwastedays=bdata.general_waste_days, generalwastefrequency=bdata.general_waste_frequency, foodwastedays=bdata.food_waste_days, foodwastefrequency=bdata.food_waste_frequency, materialsprovided=bdata.materials_provided, contact=bdata.contact)
+    return render_template("recycle.html", recycleday=bdata.recycling_day, frequency=bdata.recycling_frequency, generalwastedays=bdata.general_waste_days, generalwastefrequency=bdata.general_waste_frequency, foodwastedays=bdata.food_waste_days, foodwastefrequency=bdata.food_waste_frequency, materialsprovided=bdata.materials_provided, contact=bdata.contact)
 
 app.run()
