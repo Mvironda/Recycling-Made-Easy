c0dec70b46a453fdead3af049414087d9f3d2f33
Mvironda <vironda.madeleine@gmail.com>
2017-03-13 19:22:10 +0000

    Final Touches
    CSS, HTML and Search Box

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 from flask import Flask
-from flask_bootstrap import Bootstrap
+
 
 def create_app():
   app = Flask(__name__)
-  Bootstrap(app)
 
   return app
 
@@ -29,12 +29,13 @@ class BoroughData:
         self.contact = contact
 
 def find_BoroughData(boroughName):
-   	return borough_data.get(boroughName)
+   	return borough_data.get(boroughName.lower())
 
 def load_data():
     with open('library.csv', 'rU') as f:
         reader = csv.reader(f)
         for row in reader:
+            print row
             a_borough_data = BoroughData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
             borough_data[str(a_borough_data.name).lower()] = a_borough_data
 
@@ -45,12 +46,14 @@ app = Flask("MyApp")
 def borough():
     return render_template("recycle.html")
 
-@app.route("/<boroughName>")
-def borough_name(boroughName):
+@app.route("/boroughsearch", methods=['POST'])
+def borough_name():
+    form_data = request.form
+    boroughName = form_data['name']
     print boroughName
     load_data()
     bdata = find_BoroughData(boroughName)
     print bdata
-    return render_template("recycle.html", recycleday=bdata.recycling_day, frequency=bdata.recycling_frequency, generalwastedays=bdata.general_waste_days, generalwastefrequency=bdata.general_waste_frequency, foodwastedays=bdata.food_waste_days, foodwastefrequency=bdata.food_waste_frequency, materialsprovided=bdata.materials_provided, contact=bdata.contact)
+    return render_template("recycle.html", recycleday=bdata.recycling_day, recyclingfrequency=bdata.recycling_frequency, generalwastedays=bdata.general_waste_days, generalwastefrequency=bdata.general_waste_frequency, foodwastedays=bdata.food_waste_days, foodwastefrequency=bdata.food_waste_frequency, materialsprovided=bdata.materials_provided, contact=bdata.contact)
 
 app.run()
